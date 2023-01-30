eb7250bb9a9278d0066decdb170fd4b9c0259af7
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-01-31 00:05:14 +0530

    comment to clear

--- src/ios/withNotificationsXcodeProject.ts
+++ src/ios/withNotificationsXcodeProject.ts
@@ -315,9 +315,8 @@ async function addPushNotificationFile(
   const appPath = `${iosPath}/${appName}`;
   const getTargetFile = (filename: string) => `${appPath}/${filename}`;
 
-  console.log("Getting target file name - ", getTargetFile(file));
-console.log("Checking appPath - ", appPath);
-
+  // Check whether {file} exists in the project. If false, then add the file
+  // If {file} exists then skip and return
   if (!FileManagement.exists(getTargetFile(file))) {
     FileManagement.mkdir(appPath, {
       recursive: true,
